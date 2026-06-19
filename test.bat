6974c7edf6352cd7530cee1da2f972bcdfab5582
obycod <obycode23@gmail.com>
2026-06-20 02:32:58 +0300

    إزالة الملفات الزائدة والمؤقتة وتحديث gitignore

--- test.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-ping 127.0.0.1 -n 3 > nul
-echo Done > C:\Users\asus\OneDrive\Desktop\main\test.txt
--- test.js
+++
@@ -1,1 +0,0 @@
-let subject = 'رياضيات ج2 \\ اول متوسط \\ علي صادق'; let safeName = subject.split('\\').join('-').split('/').join('-').split(':').join('-').trim(); console.log(safeName + '.png');
