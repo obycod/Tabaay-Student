45ed13c2de5874c7fb1031c6efb2c4d1ad8193bd
obycod <obycode23@gmail.com>
2026-06-19 23:36:08 +0300

    إطلاق التحديث 7.5 لتجربة عمل شريط التحميل بعد نجاح فتح البرنامج بشكل مستقل

--- backend.py
+++ backend.py
@@ -15,7 +15,7 @@ import eel
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-APP_VERSION = "7.4"
+APP_VERSION = "7.5"
 BASE_URL_FILES = "http://pdd.xdt.mybluehost.me/update"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
