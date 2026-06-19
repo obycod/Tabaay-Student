b567dabf5a95b304b3512963d6a53a5b5c9b9c2d
obycod <obycode23@gmail.com>
2026-06-19 22:48:57 +0300

    إطلاق التحديث 6.7 لتجربة شريط التحميل الجديد

--- backend.py
+++ backend.py
@@ -13,7 +13,7 @@ import eel
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-APP_VERSION = "6.6"
+APP_VERSION = "6.7"
 BASE_URL_FILES = "http://pdd.xdt.mybluehost.me/update"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
