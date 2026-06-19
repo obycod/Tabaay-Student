380dd62d89990e5fb30e2a21eaf2fa4b7f8b75ea
obycod <obycode23@gmail.com>
2026-06-20 00:15:25 +0300

    إضافة أنميشن احترافي لأيقونة التحميل كنبضات (Pulse) وحركة ارتداد للسهم وتحديث الإصدار إلى 8.1

--- backend.py
+++ backend.py
@@ -15,7 +15,7 @@ import eel
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-APP_VERSION = "8.0"
+APP_VERSION = "8.1"
 BASE_URL_FILES = "http://pdd.xdt.mybluehost.me/update"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
